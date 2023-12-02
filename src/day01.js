2dfee13987d332a996840333a91db585df700407
Shitesh Arihunta <arihunta@gmail.com>
2023-12-02 14:27:58 +0200

    Day 01-02

--- src/day01.js
+++ src/day01.js
@@ -2,9 +2,40 @@
 const day01_01 = (input) => {
     return input.split('\n')
         .map((x) => x.replace(/[a-zA-Z]/g, "").trim())
-        .map((x) => `${x.slice(0,1)}${x.slice(-1)}`)
+        .map((x) => `${x.slice(0, 1)}${x.slice(-1)}`)
         .map((x) => parseInt(x))
         .reduce((acc, val) => acc + val, 0)
 }
 
-module.exports = day01_01
+const day01_02 = (input) => {
+    const re = RegExp('one|two|three|four|five|six|seven|eight|nine|[0-9]', 'g');
+    return input.split('\n')
+        .map((x) => x.trim())
+        .map((x) => {
+            var results = [];
+            while ( (mch = re.exec(x) ) != null ) {
+                results.push( mch[0] );
+                re.lastIndex = mch.index + 1
+            }
+            return results.map((y) => {
+                switch (y) {
+                    case "one": return "1"
+                    case "two": return "2"
+                    case "three": return "3"
+                    case "four": return "4"
+                    case "five": return "5"
+                    case "six": return "6"
+                    case "seven": return "7"
+                    case "eight": return "8"
+                    case "nine": return "9"
+                    default: return y
+                }
+            }).join('')
+        })
+        .map((x) => x.replace(/[a-zA-Z]/g, "").trim())
+        .map((x) => `${x.slice(0, 1)}${x.slice(-1)}`)
+        .map((x) => parseInt(x))
+        .reduce((acc, val) => acc + val, 0)
+}
+
+module.exports = { day01_01, day01_02 }
--- test/day01_test.js
+++ test/day01_test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const day01_01 = require("../src/day01");
+const { day01_01, day01_02 } = require("../src/day01");
 const { readFileSync } = require('fs');
 
 describe('Day 01', function () {
@@ -11,6 +11,18 @@ describe('Day 01', function () {
 
         it('solve the problem', function () {
             assert.equal(day01_01(readFileSync('./data/01', 'utf8')), 54927);
+        });
+
+    });
+
+    describe('Problem 02', function () {
+
+        it('solve example 02', function () {
+            assert.equal(day01_02(readFileSync('./data/01_02', 'utf8')), 281);
+        });
+
+        it('solve the problem', function () {
+            assert.equal(day01_02(readFileSync('./data/01', 'utf8')), 54581);
         });
 
     });
